cefc71da1cfa91ac7feb5b4a1bf78e5a2d2c4b9e
Lance <xiaowenbo2008@hotmail.com>
2020-06-15 14:11:06 +0930

    add tables, icon

---
+++ database/migrations/2020_06_10_013833_create_corks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('corks', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',255);
+            $table->text('description');
+            $table->string('size');
+            $table->double('price',8,2);
+            $table->double('cost',8,2);
+            $table->integer('current inventory');
+            $table->integer('order quantity');
+            $table->integer('need to be ordered');
+            $table->double('current inventory value',8,2);
+            $table->string('belong to',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('corks');
+    }
+}
